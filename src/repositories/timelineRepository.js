1b47886b24e95dcd055e7ae5c1057b37c1924778
taianaazevedo <taianazev@gmail.com>
2023-03-08 19:34:40 -0300

    update timeline

--- src/repositories/timelineRepository.js
+++ src/repositories/timelineRepository.js
@@ -1,8 +1,8 @@
 import db from "../config/database.js";
-import urlMetadata from "url-metadata"
+import urlMetadata from "url-metadata";
 
 export async function getTimeline() {
-    const result = await db.query(`
+  const result = await db.query(`
     SELECT 
       users.name AS user_name,
       users.img_url AS user_img_url,
@@ -15,18 +15,23 @@ export async function getTimeline() {
     LIMIT 20;
   `);
 
-  const rowsWithMetadata = await Promise.all(result.rows.map(async (row) => {
-    const { description, image, url, title } = await urlMetadata(row.post_link, { description: true, image: true, url: true, title: true });
-    return { 
-      ...row, 
-      post_description: description, 
-      post_image: image, 
-      post_url: url, 
-      post_title: title 
-    };
-  }));
+  const rowsWithMetadata = await Promise.all(
+    result.rows.map(async (row) => {
+      const { description, image, url, title } = await urlMetadata(
+        row.post_link,
+        { description: true, image: true, url: true, title: true }
+      );
+      return {
+        ...row,
+        post_description: description,
+        post_image: image,
+        post_url: url,
+        post_title: title,
+      };
+    })
+  );
 
-  console.log(rowsWithMetadata)
+  console.log(rowsWithMetadata);
 
   return rowsWithMetadata;
 }
@@ -40,6 +45,14 @@ export async function createPostByUser(url, description, userId) {
     `,
     [url, description, userId]
   );
-
+  await db.query(
+    `
+  UPDATE posts
+  SET description = $1,
+      url = $2
+  WHERE user_id = $3 
+  `,
+    [description, url, userId]
+  );
   return result;
 }
