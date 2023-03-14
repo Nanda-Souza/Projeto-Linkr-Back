11013a16c7f4739fa1b7c4ebe1d5de42424549bf
Leonan <leonanm04@gmail.com>
2023-03-13 21:05:14 -0300

    refactor: joi confirm password disabled

--- src/models/authSchema.js
+++ src/models/authSchema.js
@@ -6,13 +6,13 @@ export const signUpSchema = joi.object({
   email: joi.string().min(1).email().required(),
   img_url: joi.string().uri().min(1).required(),
   password: joi.string().min(1).required(),
-  confirmPassword: joi
-    .string()
-    .valid(joi.ref("password"))
-    .messages({
-      "any.only": "Passwords do not match",
-    })
-    .required(),
+  // confirmPassword: joi
+  //   .string()
+  //   .valid(joi.ref("password"))
+  //   .messages({
+  //     "any.only": "Passwords do not match",
+  //   })
+  //   .required(),
 });
 
 export const signInSchema = joi.object({
