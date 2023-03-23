e4fc31ed0fbefa861774ad9e82f99799324bc81a
Leonan Martins <leonanm04@gmail.com>
2023-03-23 20:15:00 -0300

    fix: delete posts wich has hashtags and likes

--- src/repositories/trendingRepository.js
+++ src/repositories/trendingRepository.js
@@ -3,7 +3,7 @@ import urlMetadata from "url-metadata";
 import { getLikeInfo } from "./likesRepository.js";
 
 export async function getTopTrends() {
-    const result = await db.query(`
+  const result = await db.query(`
         SELECT 
 		    h.name as "trendName",			
 		    COUNT(h.name) AS "threndingCount"
@@ -13,12 +13,13 @@ export async function getTopTrends() {
         GROUP BY h.name, h.id
         ORDER BY "threndingCount" DESC LIMIT 10
       `);
-  
-    return result;
-  }
 
-  export async function getPostTrends(trendName, userId) {
-    const result = await db.query(`
+  return result;
+}
+
+export async function getPostTrends(trendName, userId) {
+  const result = await db.query(
+    `
       SELECT 
         posts.created_at,
         users.name AS user_name,
@@ -34,89 +35,95 @@ export async function getTopTrends() {
       WHERE hashtags.name = $1 
       ORDER BY posts.created_at DESC
       LIMIT 20`,
-      [trendName]
+    [trendName]
+  );
+
+  const postIds = result.rows.map((i) => i.post_id);
+
+  const likesArr = await getLikeInfo(postIds, userId);
+
+  const rowsWithMetadata = await Promise.all(
+    result.rows.map(async (row, index) => {
+      const { description, image, url, title } = await urlMetadata(
+        row.post_link,
+        { description: true, image: true, url: true, title: true }
       );
-    
-    const postIds = result.rows.map((i) => i.post_id)
-    
-    const likesArr = await getLikeInfo(postIds, userId)
-    
-    const rowsWithMetadata = await Promise.all(
-      result.rows.map(async (row, index) => {
-        const { description, image, url, title } = await urlMetadata(
-          row.post_link,
-          { description: true, image: true, url: true, title: true }
-        );
-        return {
-          ...row,
-          post_description: description,
-          post_image: image,
-          post_url: url,
-          post_title: title,
-          likeInfo: likesArr[index]
-        };
-      })
-    );
-      
-  
-    return rowsWithMetadata;
+      return {
+        ...row,
+        post_description: description,
+        post_image: image,
+        post_url: url,
+        post_title: title,
+        likeInfo: likesArr[index],
+      };
+    })
+  );
+
+  return rowsWithMetadata;
+}
+
+export async function createHashtag(post_id, description) {
+  const regex = /#(\w+)/g;
+  const matches = [];
+  let match;
+
+  while ((match = regex.exec(description)) !== null) {
+    matches.push(match[1]);
   }
 
-  export async function createHashtag(post_id, description) {
-    const regex = /#(\w+)/g;
-    const matches = [];
-    let match;
+  for (let i = 0; i < matches.length; i = i + 1) {
+    try {
+      const checkHash = await db.query(
+        `SELECT id FROM hashtags WHERE name = $1;`,
+        [matches[i]]
+      );
 
-    while ((match = regex.exec(description)) !== null) {
-      matches.push(match[1]);
-    }
-    
-    for(let i = 0; i < matches.length; i = i + 1) {
-
-      try {
-
-        const checkHash = await db.query(`SELECT id FROM hashtags WHERE name = $1;`, [matches[i]]);
-    
-        if (checkHash.rowCount  === 0) {
-              const newHash = await db.query(
-                `
+      if (checkHash.rowCount === 0) {
+        const newHash = await db.query(
+          `
                 INSERT INTO hashtags 
                 (name)
                 VALUES ($1) RETURNING id
                 `,
-                [matches[i]]
-              );
+          [matches[i]]
+        );
 
-              const hashtag_id = newHash.rows[0].id
+        const hashtag_id = newHash.rows[0].id;
 
-              await db.query(
-                `
+        await db.query(
+          `
                 INSERT INTO post_hashtags 
                 (post_id, hashtag_id)
                 VALUES ($1, $2)
                 `,
-                [post_id, hashtag_id]
-              );
-              
-        }
-        else{
-          const hashtag_id = checkHash.rows[0].id
+          [post_id, hashtag_id]
+        );
+      } else {
+        const hashtag_id = checkHash.rows[0].id;
 
-          await db.query(
-            `
+        await db.query(
+          `
             INSERT INTO post_hashtags 
             (post_id, hashtag_id)
             VALUES ($1, $2)
             `,
-            [post_id, hashtag_id]
-          );
-
-        }
-      } catch (error) {
-            res.status(500).send(error)
-      }      
+          [post_id, hashtag_id]
+        );
+      }
+    } catch (error) {
+      res.status(500).send(error);
     }
-    
-    
-    
   }
+}
+
+export async function deletePostHashtags(post_id) {
+  await db.query(
+    `
+      DELETE FROM post_hashtags 
+      WHERE post_id = $1
+      `,
+    [post_id]
+  );
+
+  return;
+}
